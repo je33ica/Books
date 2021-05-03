e777c20a78c815cba9efec0f0a39d9111f6cae9f
je33ica <je331687@gmail.com>
2021-05-03 20:00:46 +0100

    search route added to index

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
 
-// Book routes
+const bookRoutes = require("./books");
+const googleRoutes = require("./search");
+// const apiRoutes = require("./api");
+
 router.use("/books", bookRoutes);
+router.use("/search", googleRoutes);
+
+// // If no API routes are hit, send the React app
+// router.use(function (req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 module.exports = router;
